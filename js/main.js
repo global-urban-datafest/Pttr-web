ebdbf2552a87791c48ac5d1fdc52d6e9b24d93b0
Xavier <xthom001@gmail.com>
2015-03-07 09:23:30 -0500

    adding in the animation plugins and other dependencies

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
+smoothScroll.init({
+    speed: 1000,
+    easing: 'easeInOutCubic',
+    offset: 0,
+    updateURL: true,
+    callbackBefore: function(toggle, anchor) {},
+    callbackAfter: function(toggle, anchor) {}
+});
 $(document).ready(function() {
     $(function() {
         $('[data-toggle="tooltip"]').tooltip()
     })
-    smoothScroll.init({
-	    speed: 1000,
-	    easing: 'easeInOutCubic',
-	    offset: 0,
-	    updateURL: true,
-	    callbackBefore: function(toggle, anchor) {},
-	    callbackAfter: function(toggle, anchor) {}
-	});
+
 })
