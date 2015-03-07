15a6bce1fd8f6b2ccf6b1a7c83a2551232f9390e
Xavier <xthom001@gmail.com>
2015-03-07 08:43:18 -0500

    added in the skroll plugin and created the profile and search preference page.. they need styling

--- js/main.js
+++ js/main.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
     $(function() {
         $('[data-toggle="tooltip"]').tooltip()
     })
+    smoothScroll.init({
+	    speed: 1000,
+	    easing: 'easeInOutCubic',
+	    offset: 0,
+	    updateURL: true,
+	    callbackBefore: function(toggle, anchor) {},
+	    callbackAfter: function(toggle, anchor) {}
+	});
 })
