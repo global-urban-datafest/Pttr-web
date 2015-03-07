57e10ed7b13f0087df37c026f3228fe3422c6b2c
Alastair Paragas <alastairparagas@gmail.com>
2015-03-06 23:05:58 -0500

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+(function () {
+    
+    'use strict';
+
+    var gulp = require('gulp'),
+        sass = require('gulp-sass'),
+        uglify = require('gulp-uglify'),
+        concat = require('gulp-concat'),
+        plumber = require('gulp-plumber');
+
+    gulp.task('sass', function () {
+        return gulp.src('scss/**/*.scss')
+                    .pipe(plumber())
+                    .pipe(sass({outputStyle: 'compressed'}))
+                    .pipe(gulp.dest('css'));
+    });
+
+    gulp.task('watch', function () {
+        gulp.watch('scss/**/*.scss', ['sass']);
+    });
+    
+    gulp.task('default', ['watch']);
+    
+}());
