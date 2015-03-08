de5c9c39603a570a31728485ece0524a6f3454c2
Xavier <xthom001@gmail.com>
2015-03-07 19:33:52 -0500

    merge

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ var wow = new WOW(
     boxClass:     'wow',      // animated element css class (default is wow)
     animateClass: 'animated', // animation css class (default is animated)
     duration:     14,          //duration of chosen animation
-    offset:       0,        // distance to the element when triggering the animation (default is 0)
+    offset:       200,        // distance to the element when triggering the animation (default is 0)
     mobile:       true,       // trigger animations on mobile devices (default is true)
     live:         true,       // act on asynchronously loaded content (default is true)
     callback:     function(box) {
