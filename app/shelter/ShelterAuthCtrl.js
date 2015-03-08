46b83e75c2b045f7966c91196f5e2a7006d71c47
Alastair Paragas <alastairparagas@gmail.com>
2015-03-08 16:12:11 -0400

    Fixed stuff

--- app/shelter/ShelterAuthCtrl.js
+++ app/shelter/ShelterAuthCtrl.js
@@ -3,7 +3,28 @@
     
     var angular = window.angular;
     
-    angular.module('pttr.shelter').controller('ShelterAuthCtrl', ['$scope', function ($scope) {
+    angular.module('pttr.shelter').controller('ShelterAuthCtrl', ['$state', '$scope', 'AuthService', function ($state, $scope, AuthService) {
+        
+        $scope.userActions = {};
+        
+        AuthService.login().then(
+            function () {
+                console.log(AuthService.isLoggedIn());
+            }
+        );
+        
+        $scope.userActions.logout = function () {
+            AuthService.logout();
+            $state.go('noAuth.home');
+        };
+        
+        $scope.userActions.editProfile = function () {
+            $state.go('individualAuth.editProfile');
+        };
+        
+        $scope.userActions.saveProfile = function () {
+            
+        };
         
     }]);
     
