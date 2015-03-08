d6f0e20872d0f59c63b54f6020ab357263d5f67c
Xavier <xthom001@gmail.com>
2015-03-08 15:53:59 -0400

    okay run

--- app/individual/DashboardIndividualCtrl.js
+++ app/individual/DashboardIndividualCtrl.js
@@ -3,56 +3,58 @@
     
     var angular = window.angular;
     
-    angular.module('pttr.individual').controller('DashboardIndividualCtrl', ['$scope', function ($scope) {
+    angular.module('pttr.individual').controller('DashboardIndividualCtrl', ['$scope', '$http', function ($scope,$http) {
+        $http.get('js/animalinfo.json').success(function(data){
+        		$scope.animals = data;
+        });
         $scope.liked = false;
-        $scope.animals = [
-        	{
-        		name: "monkey",
-        		type: "beast",
-        		description: "Donkey kong like animal"
-        	},
-        	{
-        		name: "monkey",
-        		type: "beast",
-        		description: "Donkey kong like animal"
-        	},
-        	{
-        		name: "monkey",
-        		type: "beast",
-        		description: "Donkey kong like animal"
-        	},
-        	{
-        		name: "monkey",
-        		type: "beast",
-        		description: "Donkey kong like animal"
-        	},
-        	{
-        		name: "monkey",
-        		type: "beast",
-        		description: "Donkey kong like animal"
-        	},
-        	{
-        		name: "monkey",
-        		type: "beast",
-        		description: "Donkey kong like animal"
-        	},
-        	{
-        		name: "monkey",
-        		type: "beast",
-        		description: "Donkey kong like animal"
-        	},
-        	{
-        		name: "monkey",
-        		type: "beast",
-        		description: "Donkey kong like animal"
-        	},
-        	{
-        		name: "monkey",
-        		type: "beast",
-        		description: "Donkey kong like animal"
-        	}
-
-        ]
+        // $scope.animals = [
+        // 	{
+        // 		name: "monkey",
+        // 		type: "beast",
+        // 		description: "Donkey kong like animal"
+        // 	},
+        // 	{
+        // 		name: "monkey",
+        // 		type: "beast",
+        // 		description: "Donkey kong like animal"
+        // 	},
+        // 	{
+        // 		name: "monkey",
+        // 		type: "beast",
+        // 		description: "Donkey kong like animal"
+        // 	},
+        // 	{
+        // 		name: "monkey",
+        // 		type: "beast",
+        // 		description: "Donkey kong like animal"
+        // 	},
+        // 	{
+        // 		name: "monkey",
+        // 		type: "beast",
+        // 		description: "Donkey kong like animal"
+        // 	},
+        // 	{
+        // 		name: "monkey",
+        // 		type: "beast",
+        // 		description: "Donkey kong like animal"
+        // 	},
+        // 	{
+        // 		name: "monkey",
+        // 		type: "beast",
+        // 		description: "Donkey kong like animal"
+        // 	},
+        // 	{
+        // 		name: "monkey",
+        // 		type: "beast",
+        // 		description: "Donkey kong like animal"
+        // 	},
+        // 	{
+        // 		name: "monkey",
+        // 		type: "beast",
+        // 		description: "Donkey kong like animal"
+        // 	}
+        // ]
        
         // var viewAnimal = function(name,type){
 
